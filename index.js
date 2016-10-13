b621d1630e60a391bfc1da1607752f5c97fa3121
miusuncle <miusuncle@gmail.com>
2016-10-13 16:28:19 +0800

    update: index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 module.exports = {
     config: require('./config'),
+
     LoginService: require('./lib/auth/login-service'),
     LoginServiceError: require('./lib/auth/login-service-error'),
+
+    TunnelService: require('./lib/tunnel/tunnel-service'),
 };
