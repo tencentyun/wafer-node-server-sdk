b011e74a4192757bfa485cb0f509d4ba45a6dcea
miusuncle <miusuncle@gmail.com>
2016-10-13 20:35:48 +0800

    rename: `SecretKey` => `TunnelSignatureKey`

--- config.js
+++ config.js
@@ -10,9 +10,6 @@ exports = options => {
     });
 };
 
-// SDK 密钥，该密钥需要保密
-exports.SecretKey = '';
-
 // 当前使用 SDK 服务器的主机，该主机需要外网可访问
 exports.ServerHost = '';
 
@@ -21,5 +18,8 @@ exports.AuthServerHost = '';
 
 // 信道服务器服务地址
 exports.TunnelServerHost = '';
+
+// 信道服务签名密钥，该密钥需要保密
+exports.TunnelSignatureKey = '';
 
 module.exports = exports;
