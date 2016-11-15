0bc73765f96af5a91db672854e53a40aab62dc20
liamhuang <liamhuang@tencent.com>
2016-11-15 16:06:33 +0800

    修改了格式

--- test/test-server/routes/auth.js
+++ test/test-server/routes/auth.js
@@ -2,7 +2,7 @@
 
 class Router extends RouterBase {
     handle() {
-        let interfaceName, code, encrypt_data,iv, id, skey;
+        let interfaceName, code, encrypt_data, iv, id, skey;
 
         try {
             // ES6 syntax (not work for node v4)
@@ -17,7 +17,7 @@ class Router extends RouterBase {
             interfaceName = body.interface.interfaceName;
             code = body.interface.para.code;
             encrypt_data = body.interface.para.encrypt_data;
-            iv           = body.interface.para.id;
+            iv = body.interface.para.id;
             id = body.interface.para.id;
             skey = body.interface.para.skey;
         } catch (err) {
@@ -26,7 +26,7 @@ class Router extends RouterBase {
 
         switch (interfaceName) {
         case 'qcloud.cam.id_skey':
-            this.handleLoginRequest(code, encrypt_data ,iv);
+            this.handleLoginRequest(code, encrypt_data, iv);
             break;
 
         case 'qcloud.cam.auth':
